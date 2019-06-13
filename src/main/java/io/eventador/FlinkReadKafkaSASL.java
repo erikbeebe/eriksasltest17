950f429272e3939e64a2db9131f23e2adc16d22b
Erik Beebe <erik@beebe.cc>
2019-06-13 16:58:39 -0500

    testing

--- src/main/java/io/eventador/FlinkReadKafkaSASL.java
+++ src/main/java/io/eventador/FlinkReadKafkaSASL.java
@@ -21,17 +21,6 @@ public class FlinkReadKafkaSASL {
                 return;
             }
 
-            // This job expects to be able to find your truststore in the resources/ directory, named
-            // "eventador_truststore.jks".  To create the trust store, simply download the Deployment
-            // CA certificate from your deployment (click on the Apache Kafka link for your deployment,
-            // then "SASL Users", then "Download Certificate".  You will end up with a filename that looks
-            // like "xxxx-ca-cert.pem".  Use keytool to generate a truststore using this file:
-            // keytool -keystore eventador_truststore.jks -alias CARoot -import -file xxxx-ca-cert.pem -storepass YOUR_PASSWORD -noprompt
-            // Be sure to note the password you protect your truststore with, and pass it to your
-            // job in the --truststore.password argument.
-            //ClassLoader classLoader = FlinkReadKafkaSASL.class.getClassLoader();
-            //String truststore_path = classLoader.getResource("eventador_truststore.jks").getPath();
-
             // Configure ScramLogin via jaas
             String module = "org.apache.kafka.common.security.scram.ScramLoginModule";
             String jaasConfig = String.format("%s required username=\"%s\" password=\"%s\";", module, params.getRequired("username"), params.getRequired("password"));
@@ -47,9 +36,6 @@ public class FlinkReadKafkaSASL {
             kparams.setProperty("security.protocol", "SASL_SSL");
             kparams.setProperty("sasl.mechanism", "SCRAM-SHA-256");
             kparams.setProperty("sasl.jaas.config", jaasConfig);
-            kparams.setProperty("ssl.truststore.type", "jks");
-            kparams.setProperty("ssl.truststore.location", truststore_path);
-            kparams.setProperty("ssl.truststore.password", params.getRequired("truststore.password"));
 
             // Setup streaming environment
             StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
